38fe0b7871ee5ccbe99151520091eede071a3e56
Sherie Carter <sherie1.carter@gmail.com>
2016-10-31 17:21:27 -0700

    created controllers

--- esite/config/routes.rb
+++ esite/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/faqs'
+
+  get 'page/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

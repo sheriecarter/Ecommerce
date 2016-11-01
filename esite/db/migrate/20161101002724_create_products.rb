df50f72b1f89b2f9ccd69d77d78ffeb0e9119d70
Sherie Carter <sherie1.carter@gmail.com>
2016-10-31 17:33:52 -0700

    creates cart

--- esite/db/migrate/20161101002724_create_products.rb
+++ esite/db/migrate/20161101002724_create_products.rb
@@ -1,7 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-      t.stringdescription :title
+      t.string :description
+      t.string :title
       t.string :image_url
       t.decimal :price
       t.string :category
